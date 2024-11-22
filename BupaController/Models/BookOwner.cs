4303eb63d7ea9b5fabdadcbe214f861432906646
mahammadsadiqueDodamani <msd996499@gmail.com>
2024-11-22 10:39:45 +0530

    Delete BupaController/Models/BookOwner.cs

--- BupaController/Models/BookOwner.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Bupa.Models
-{
-    public class BookOwner
-    {
-        public string Name { get; set; }
-        public int Age { get; set; }
-        public List<Book> Books { get; set; }
-    }
-}
